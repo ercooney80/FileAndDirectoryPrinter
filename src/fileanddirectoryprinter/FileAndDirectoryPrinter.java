691a07bb16d9c16834931d1e14a7918c56ae33be
ercooney80 <ercooney80@gmail.com>
2014-07-12 13:43:51 -0400

    Added getParent() and isValid(String parent) functions.  Allows user to specify parent directory

--- src/fileanddirectoryprinter/FileAndDirectoryPrinter.java
+++ src/fileanddirectoryprinter/FileAndDirectoryPrinter.java
@@ -1,14 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The simple program will print the files and subdirectories of a folder in Windows    
  */
 
 package fileanddirectoryprinter;
 
+import java.io.File;
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author user1
+ * @author Edward Cooney
  */
 public class FileAndDirectoryPrinter {
 
@@ -16,7 +17,37 @@ public class FileAndDirectoryPrinter {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // Get parent folder name from user
+        String parent = getParent();
+    }
+    
+    /**
+     * Get the parent directory the user would like to start printing from
+     * @return the parent directory 
+     */
+    private static String getParent() {
+        String parent;
+        parent = JOptionPane.showInputDialog(
+                "Enter the location of the parent folder:");
+        
+        while (!isValid(parent)) {
+            JOptionPane.showMessageDialog(null, "That directory cannot be found.  Please check your input:\n" + parent);
+            
+            parent = JOptionPane.showInputDialog(
+                "Enter the location of the parent folder:");
+        }    
+        return parent;
+    }
+    
+    /**
+     * Check that the user defined parent directory exists.
+     * @param parent The user defined parent directory
+     * @return TRUE if the directory exists, otherwise FALSE
+     */
+    private static boolean isValid(String parent) {
+        File file = new File(parent);
+        return file.exists();
     }
     
 }
